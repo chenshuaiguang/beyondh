4a4733c86c6cc4cf16130176c6734540ed6e49f3
chenshuaiguang <chenshuaiguang@meituan.com>
2018-11-13 09:23:19 +0800

    增加空格

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ App({
       wx.login({
         success: function () {
           wx.getUserInfo({
+            
             success: function (res) {
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
