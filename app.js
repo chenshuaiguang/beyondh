c5b78d5bf75d6c5ace85c420d58a15265dadc5d6
chenshuaiguang <chenshuaiguang@meituan.com>
2018-11-13 09:32:22 +0800

    增加注释

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ App({
       wx.login({
         success: function () {
           wx.getUserInfo({
-            success: function (res) {
+            success: function (res) {//111
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
             }
